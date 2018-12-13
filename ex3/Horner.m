b7f058f25e50226bd886fa073214bc68e377de02
Mateusz <bieganski.m@wp.pl>
2018-12-13 22:44:33 +0100

    initial commit

---
+++ ex3/Horner.m
@@ -0,0 +1,5 @@
+function v = Horner(c, x, z)
+  # c, x - współczynniki wielomianu w bazie Newtona i węzły 
+  # v - wartości wielomianu w punktach wektora z
+  printf("ssij")
+endfunction
---
+++ ex3/interpNewton.m
@@ -0,0 +1,5 @@
+function c = interpNewton(x, y)
+  # x, y - wektory określające węzły i wartości w węzłach
+  # c - wektor współczynników wielomianu w bazie Newtona
+  # TODO
+endfunction
