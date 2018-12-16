b4b9b07dce38cd6d8aa0be4bf13902b223e4fdd3
Mateusz <bieganski.m@wp.pl>
2018-12-16 15:09:55 +0100

    first done

--- ex3/Horner.m
+++ ex3/Horner.m
@@ -1,5 +1,16 @@
 function v = Horner(c, x, z)
-  # c, x - współczynniki wielomianu w bazie Newtona i węzły 
+  # c, x - współczynniki wielomianu w bazie Newtona i węzły
+  # z - punkty, w których wartości poszukujemy
   # v - wartości wielomianu w punktach wektora z
-  printf("ssij")
+  # 
+  # zakładamy, że dostaliśmy wektory poziome
+  
+  n = columns(x);
+  assert(n > 1); # TODO
+  res = 0;
+  v = zeros(size(z))
+  for i = (n-1):-1:1
+    v .*= (z - x(i)) 
+    v += c(i) 
+  endfor
 endfunction
